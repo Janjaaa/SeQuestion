d55d77919b1ec24ffcfce7b004626d3054180c26
Janjaaa <jpao76@gmail.com>
2021-09-09 09:15:11 +0100

    Added port configs

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const route = require('./route')
 const path = require('path')
+const PORT = process.env.PORT || '8080'
 
 const server = express()
 
@@ -8,6 +9,10 @@ server.set('view engine', 'ejs')
 server.use(express.static('public'))
 server.set('views', path.join(__dirname, 'views'))
 
-server.use(express.urlencoded({ extended: true }))
+server.use(express.urlencoded({extended: true}))
 
 server.use(route)
+
+server.set('port', PORT)
+
+server.listen('port', () => console.log('Rodando'))
